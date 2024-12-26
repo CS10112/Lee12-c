535e9c5bc8f9dded23942230f47442226b39e891
CS10112 <313012@s.lhvs.tyc.edu.tw>
2024-12-26 13:42:27 +0800

    Update 20241226類別成員.cpp

--- 20241226類別成員.cpp
+++ 20241226類別成員.cpp
@@ -21,3 +21,26 @@ class myTime{
 void myTime::printTime(){
 	cout<<hour<<":"<<minute<<"\n";
 }
+void myTime::printTime(){
+	cout<<hour<<":"<<minute<<"\n";
+}
+int square(int no){return no*no;}
+double square(double no){return no*no;}
+
+int main()
+{
+	myTime open, close;
+	int h,m;
+	open.setTime(10,30);
+	close.setTime(17);
+	cout <<"開張時間:";
+	open.printTime();
+	cout<<"結束時間:";
+	close.printTime();
+	int no1 = 14;
+	double no2 = 35.4;
+	cout<<no1<<"*"<<no2<<" = "<<
+	square(no1)<<"\n";
+	cout<<no2<<"*"<<no2<<" = "<<
+	square(no2)<<"\n"; 
+}
